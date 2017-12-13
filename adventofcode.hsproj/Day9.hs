18fe5528047832533fa442b9308dfd69e0c0d30f
Giovanni Frigo <john.frigo92@gmail.com>
2017-12-13 10:51:52 +0100

    Day 9 🎁

---
+++ adventofcode.hsproj/Day9.hs
@@ -0,0 +1,18 @@
+-- http://adventofcode.com/2017/day/9
+
+module Day9 where
+    
+parse :: [Char] -> Int -> Bool -> Int
+
+parse ('{':xs) openedGroups False       = 0 + parse xs (openedGroups + 1) False -- '{' outside garbage opens a new group
+parse ('}':xs) openedGroups False       = openedGroups + parse xs (openedGroups - 1) False -- '}' outside garbage closes a group: sum score
+parse ('!':xs) openedGroups garbageMode = 0 + parse (tail xs) openedGroups garbageMode -- drop next character
+parse ('<':xs) openedGroups garbageMode = 0 + parse xs openedGroups True -- enterning garbage (if not already inside it)
+parse ('>':xs) openedGroups garbageMode = 0 + parse xs openedGroups False -- exiting garbage
+parse ( _ :xs) openedGroups garbageMode = 0 + parse xs openedGroups garbageMode -- non relevant characters
+parse []       _            _           = 0
+
+process :: [Char] -> Int
+process input = parse input 0 False
+
+solve input = process input
---
+++ adventofcode.hsproj/Day9p2.hs
@@ -0,0 +1,17 @@
+-- http://adventofcode.com/2017/day/9
+
+module Day9p2 where
+    
+parse :: [Char] -> Bool -> Int
+
+parse ('!':xs) garbageMode = 0 + parse (tail xs) garbageMode -- drop next character
+parse ('<':xs) False       = 0 + parse xs True  -- enterning garbage (opening!! char does not count) 
+parse ('>':xs) True        = 0 + parse xs False -- exiting garbage (closing char does not count) 
+parse ( _ :xs) True        = 1 + parse xs True  -- inside garbage: count character
+parse ( _ :xs) False       = 0 + parse xs False -- else skip it
+parse []       _           = 0
+
+process :: [Char] -> Int
+process input = parse input False
+
+solve input = process input
